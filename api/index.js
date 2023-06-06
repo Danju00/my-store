bb4185545c33cc448481bf057a0ddcf63a95542f
Danju00 <leinad9265@gmail.com>
2023-06-06 02:00:55 -0600

    cambios

--- api/index.js
+++ api/index.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:5500'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
@@ -33,8 +33,6 @@ routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
-
 
 
 app.listen(port, () => {
